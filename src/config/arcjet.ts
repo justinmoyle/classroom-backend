be9db5cc8463968555481ecd41627f203117b831
justinmoyle <195149107+justinmoyle@users.noreply.github.com>
2026-01-29 23:53:04 -0500

    configure apm insight node agent settings

--- src/config/arcjet.ts
+++ src/config/arcjet.ts
@@ -3,26 +3,27 @@ import arcjet, { detectBot, shield, slidingWindow } from '@arcjet/node';
 let aj: ReturnType<typeof arcjet> | undefined;
 
 if (process.env.ARCJET_KEY) {
-    aj = arcjet({
-        key: process.env.ARCJET_KEY,
-        rules: [
-            shield({mode: 'LIVE'}),
-            detectBot({
-                mode: 'LIVE',
-                allow: [
-                    'CATEGORY:SEARCH_ENGINE',
-                    'CATEGORY:PREVIEW',
-                ],
-            }),
-            slidingWindow({
-                mode: 'LIVE',
-                interval: '1m',
-                max: 60,
-            }),
+  aj = arcjet({
+    key: process.env.ARCJET_KEY,
+    rules: [
+      shield({ mode: 'LIVE' }),
+      detectBot({
+        mode: 'LIVE',
+        allow: [
+          'CATEGORY:SEARCH_ENGINE',
+          'CATEGORY:PREVIEW',
         ],
-    });
+      }),
+      slidingWindow({
+        mode: 'LIVE',
+        interval: '1m',
+        max: 60,
+      }),
+    ],
+    proxies: ['127.0.0.1'],
+  });
 } else if (process.env.NODE_ENV !== 'test') {
-    throw new Error('ARCJET_KEY env is required');
+  throw new Error('ARCJET_KEY env is required');
 }
 
 export default aj;
